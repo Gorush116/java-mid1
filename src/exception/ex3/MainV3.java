7a42e760a6eef46642c0b3cd62ccbcfc4cdfd1a5
Gorush116 <gorush34@gmail.com>
2024-06-26 16:27:20 +0900

    언체크 예외를 사용하여 공통적으로 예외 처리하는 부분을 생성한 뒤 분기하여 처리 / try-with-resources는 try를 빠져나가는 순간 자원을 반납한다

--- src/exception/ex3/MainV3.java
+++ src/exception/ex3/MainV3.java
@@ -1,10 +1,13 @@
 package exception.ex3;
+
+import exception.ex3.exception.NetworkClientExceptionV3;
+
 import java.util.Scanner;
 
 public class MainV3 {
 
     public static void main(String[] args) throws NetworkClientExceptionV3 {
-        NetworkServiceV3_1 networkServiceV3_1 = new NetworkServiceV3_1();
+        NetworkServiceV3_2 networkServiceV3_2 = new NetworkServiceV3_2();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -14,7 +17,7 @@ public class MainV3 {
                 break;
             }
 
-            networkServiceV3_1.sendMessage(input);
+            networkServiceV3_2.sendMessage(input);
             System.out.println();
         }
         System.out.println("프로그램을 정상 종료합니다.");
